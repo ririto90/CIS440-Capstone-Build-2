dca5d373572ee83dafc695e727f589a9c3835474
aadevrie <aadevrie@asu.edu>
2023-04-13 13:42:31 -0700

    home

--- html/home.php
+++ html/home.php
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary">Login</button>
-            <a href="./html/registration.php" class="btn btn-primary">Create Account</a>
+            <a href="./registration.php" class="btn btn-primary">Create Account</a>
         </form>
     </div>
     </main>
