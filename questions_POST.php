a7b00003c5208f365f4c934d7ba4a7b22129b5cb
kvnwin <jnguye91@asu.edu>
2023-04-17 02:25:00 -0700

    My Changes

--- questions_POST.php
+++ questions_POST.php
@@ -1,5 +1,4 @@
 <?php
-// Connect to the database
 $host = "107.180.1.16:3306";
 $username = "sprc2023team2";
 $password = "sprc2023team2";
@@ -7,17 +6,15 @@ $dbname = "sprc2023team2";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get the form data
 $post_title = $_POST['post-title'];
 $post_content = $_POST['post-content'];
 $post_author = $_POST['post-author'];
 
-// Insert the form data into the table
 $sql = "INSERT INTO discussion_questions (post_title, post_content, post_author) VALUES ('$post_title', '$post_content', '$post_author')";
 
 if (mysqli_query($conn, $sql)) {
@@ -26,7 +23,6 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-// Close the database connection
 mysqli_close($conn);
 
 include('html/disscussion_board.php')
--- replies_POST.php
+++ replies_POST.php
@@ -1,5 +1,4 @@
 <?php
-// Connect to the database
 $host = "107.180.1.16:3306";
 $username = "sprc2023team2";
 $password = "sprc2023team2";
@@ -7,17 +6,15 @@ $dbname = "sprc2023team2";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get the form data
 $post_ID = $_POST['question_id'];
 $post_content = $_POST['content'];
 $post_author = $_POST['author'];
 
-// Insert the form data into the table
 $sql = "INSERT INTO discussion_replies (question_id, author, content) VALUES ('$post_ID', '$post_author', '$post_content')";
 
 echo $sql;
@@ -28,7 +25,6 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-// Close the database connection
 mysqli_close($conn);
 
 include('html/disscussion_board.php')
